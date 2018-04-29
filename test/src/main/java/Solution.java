4cb67b46f51b2b9c51501089104e8202fd231046
Ksenia <hitriysprut@gmail.com>
2018-04-29 21:10:05 +0300

    task2

--- test/src/main/java/Solution.java
+++ test/src/main/java/Solution.java
@@ -7,11 +7,33 @@ public class Solution
 {
     public static void main(String args[]) throws Exception {
 
-        Document doc = Jsoup.connect("https://jsoup.org/download").get();
-        Elements elms = doc.getElementsByTag("h2");
-        for ( Element text : elms)
+        Document doc = Jsoup.connect("http://zakupki.gov.ru/epz/main/public/home.html").get();
+        Elements elms = doc.getElementsByAttribute("href");
+        String catLink = null;
+        for ( Element el : elms)
         {
-            System.out.println(text.text());
+            if (el.text().contains("Каталог")){
+                catLink=el.attr("href");
+
+                System.out.println(catLink);
+
+            }
         }
+        doc = Jsoup.connect("http://zakupki.gov.ru"+catLink).get();
+        //System.out.println(doc);
+        elms = doc.getElementsByClass("descriptTenderTd");
+        Element el = elms.first();
+        String orderLink =el.getElementsByAttribute("href").first().attr("href");
+
+        doc=Jsoup.connect("http://zakupki.gov.ru"+orderLink).get();
+        //System.out.println(doc);
+        el = doc.getElementsByClass("ktruNsiDescr").first();
+        //System.out.println("\n"+el);
+        elms = el.getElementsByTag("td");
+        String name = elms.first().text();
+        String value = elms.last().text();
+
+        System.out.println(name+":\n"+value);
+
     }
 }
